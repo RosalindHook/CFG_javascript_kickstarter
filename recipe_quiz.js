e32f4b32626b513f1c3b248c9a81f01ae0625515
Ros <123762126+RosalindHook@users.noreply.github.com>
2023-04-09 15:57:18 +0200

    Update recipe_quiz.js
    Signed-off-by: Ros <123762126+RosalindHook@users.noreply.github.com>

--- recipe_quiz.js
+++ recipe_quiz.js
@@ -1,44 +1,25 @@
-// takeQuiz function
-function takeQuiz(score) {
+// quiz function
+function quiz() {
 
-  if (score >= 8) {
-  console.log("You should make lasagna tonight!\nThis classic Italian dish is perfect for when you're in the mood for something meaty and savoury. With layers of pasta, cheese, and tomato sauce, lasagna is always a crowd pleaser.");
-  }
-  
-  else if (score >= 5) {
-    console.log("You should make cake tonight!\nWhether you're in the mood for something chocolatey or fruity, there's a cake recipe out there for you. Plus, making a cake from scratch is a great way to impress your friends and family.");
-  } 
-  
-  else {
-    console.log("You should make pizza tonight!\nThis versatile dish can be customised with your favorite toppings, making it perfect for when you're in the mood for something savoury. Plus, it's quick and easy to make, so you can have dinner on the table in no time.");
-  }
-};
+    let type = prompt("What type of food are you in the mood for? Please enter 1- Something savoury; 2- Something sweet; 3- Something meaty");
+    let time = prompt("How much time do you have to cook? Please enter 1- less than 30 minutes; 2- 30-60 minutes; 3- more than 60 minutes");
+    let ingredients = prompt("What ingredients do you have to hand? Please enter 1- flour, tomato sauce, cheese, cherry tomatoes; 2- flour, sugar, eggs and butter; 3 - pasta, ground beef and tomato sauce");
+    let score = parseInt(type) + parseInt(time) + parseInt(ingredients);
+
+    if (score >= 8) {
+        alert("You should make lasagna tonight! This classic Italian dish is perfect for when you're in the mood for something meaty and savoury. With layers of pasta, cheese, and tomato sauce, lasagna is always a crowd-pleaser.");
+      } 
+    else if (score >= 5) {
+        alert("You should make cake tonight! Whether you're in the mood for something chocolatey or fruity, there's a cake recipe out there for you. Plus, making a cake from scratch is a great way to impress your friends and family.");
+      } 
+    else {
+        alert("You should make pizza tonight! This versatile dish can be customised with your favorite toppings, making it perfect for when you're in the mood for something savoury. Plus, it's quick and easy to make, so you can have dinner on the table in no time.");
+      }
+}
 
 // event listener (click on quiz button in html file to get the pop up quiz function)
-
 let quizButton = document.getElementById("quiz-button");
-quizButton.addEventListener("click", function() {
-  let type = prompt("What type of food are you in the mood for?\nPlease enter 1- Something savoury;\n2- Something sweet;\n3- Something meaty"); 
-  let time = prompt("How much time do you have to cook?\nPlease enter 1- less than 30 minutes;\n2- 30-60 minutes;\n3- more than 60 minutes"); 
-  let ingredients = prompt(" "What ingredients do you have to hand?\nPlease enter 1- flour, tomato sauce, cheese, cherry tomatoes;\n2- flour, sugar, eggs and butter;\n3- pasta, ground beef and tomato sauce");  
-  let score = parseInt(type) + parseInt(time) + parseInt(ingredients);
-
-      // Check if any input is null or empty
-  if (type == null || type.trim() === "" || time == null || time.trim() === "" || ingredients == null || ingredients.trim() === "") {
-    alert("Please answer all questions before submitting.");
-    return;
-  }
-
-  // Check if input is a number
-  if (isNaN(type) || isNaN(time) || isNaN(ingredients)) {
-    alert("Please enter a number for all questions.");
-    return;
-  }
-  
-  takeQuiz(score);
-});
-
-
+quizButton.addEventListener("click", quiz)
 
 
 // welcome function
