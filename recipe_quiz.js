3c17fc5613a66351e6c0c23a61e5dfbc7fe45a5b
Ros <123762126+RosalindHook@users.noreply.github.com>
2023-04-09 13:46:50 +0200

    Update recipe_quiz.js
    Signed-off-by: Ros <123762126+RosalindHook@users.noreply.github.com>

--- recipe_quiz.js
+++ recipe_quiz.js
@@ -1,14 +1,16 @@
 // takeQuiz function
-function takeQuiz(type, time, ingredients) {
-  let score = parseInt(type) + parseInt(time) + parseInt(ingredients);
+function takeQuiz(score) {
+
   if (score >= 8) {
-    alert("You scored " + score + "!");
-  } 
+  console.log("You should make lasagna tonight! This classic Italian dish is perfect for when you're in the mood for something meaty and savoury. With layers of pasta, cheese, and tomato sauce, lasagna is always a crowd pleaser.");
+  }
+  
   else if (score >= 5) {
-    alert("You should make cake tonight! Whether you're in the mood for something chocolatey or fruity, there's a cake recipe out there for you. Plus, making a cake from scratch is a great way to impress your friends and family.");
+    console.log("You should make cake tonight! Whether you're in the mood for something chocolatey or fruity, there's a cake recipe out there for you. Plus, making a cake from scratch is a great way to impress your friends and family.");
   } 
+  
   else {
-    alert("You should make pizza tonight! This versatile dish can be customised with your favorite toppings, making it perfect for when you're in the mood for something savoury. Plus, it's quick and easy to make, so you can have dinner on the table in no time.");
+    console.log("You should make pizza tonight! This versatile dish can be customised with your favorite toppings, making it perfect for when you're in the mood for something savoury. Plus, it's quick and easy to make, so you can have dinner on the table in no time.");
   }
 };
 
@@ -21,7 +23,9 @@ quizButton.addEventListener("click", function() {
   let time = prompt("How much time do you have to cook? Please enter 1- less than 30 minutes; 2- 30-60 minutes; 3- more than 60 minutes"); 
   let ingredients = prompt("What ingredients do you have to hand? Please enter 1- flour, tomato sauce, cheese, cherry tomatoes; 2- flour, sugar, eggs and butter; 3 - pasta, ground beef and tomato sauce");
   
-  takeQuiz(type, time, ingredients);
+  let score = parseInt(type) + parseInt(time) + parseInt(ingredients);
+  
+  takeQuiz(score);
 });
 
 
