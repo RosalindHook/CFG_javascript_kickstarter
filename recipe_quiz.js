f66a096bc57d9ed0aefb4eccf4de2c0bd7c39a0b
Ros <123762126+RosalindHook@users.noreply.github.com>
2023-04-09 13:32:34 +0200

    Update recipe_quiz.js
    Signed-off-by: Ros <123762126+RosalindHook@users.noreply.github.com>

--- recipe_quiz.js
+++ recipe_quiz.js
@@ -14,10 +14,22 @@ function takeQuiz(type, time, ingredients) {
 // event listener (click on quiz button in html file to get the pop up quiz function)
 
 let quizButton = document.getElementById("quiz-button");
-quizButton.addEventListener("click", function() {
+quizButton.addEventListener("click", takeQuiz() {
   let type = prompt("What type of food are you in the mood for? Please enter 1- Something savoury; 2- Something sweet; 3- Something meaty");
   let time = prompt("How much time do you have to cook? Please enter 1- less than 30 minutes; 2- 30-60 minutes; 3- more than 60 minutes"); 
   let ingredients = prompt("What ingredients do you have to hand? Please enter 1- flour, tomato sauce, cheese, cherry tomatoes; 2- flour, sugar, eggs and butter; 3 - pasta, ground beef and tomato sauce");
   
   takeQuiz(type, time, ingredients);
 });
+
+
+
+// welcome function
+function welcome() {
+    let name = prompt("What is your name?");
+    alert("Hello, " + name + "! I think you'll be a great chef soon!");
+}
+
+//event listener
+let welcomeFutureButton = document.getElementById("welcome-button");
+welcomeFutureButton.addEventListener("click", welcome);
